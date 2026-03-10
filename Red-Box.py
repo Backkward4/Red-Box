527b4a6fe09d37e97e32455e0785563b7f89b30a
Backkward <backkward4@gmail.com>
2026-03-10 13:24:29 -0500

    Added comments for clarity (and if I forget what something does)

--- Red-Box.py
+++ Red-Box.py
@@ -18,6 +18,7 @@ gravity = Vector2(0, -9.81)
 
 clock = pygame.time.Clock()
 
+#   custom class for the player, might be simplified for all objects to use polygon instead
 class playerBox:
     def __init__(self, rect):
         self.size = 40
@@ -28,25 +29,33 @@ class playerBox:
 
 redbox = playerBox(0)
 
-# Main game loop
+
+
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT: # Exit when the window is closed
             running = False
-            
+
+    #   world center
     wc = center + camera
     dT = clock.tick(60) / 1000.0
 
+    #   applies gravity constantly
     redbox.velocity -= gravity
-    
+
+    #   background
     screen.fill((110, 179, 210))
     rb_screen = redbox.rect.move(redbox.pos.x + wc.x, redbox.pos.y + wc.y)
     pygame.draw.rect(screen, (131, 31, 31), rb_screen)
-    
+
+    #   creates the border on the player
     redbox_inner = rb_screen.inflate(-10, -10)
     pygame.draw.rect(screen, (251, 53, 38), redbox_inner)
 
+    #   world center box (for reference, will be removed once maps work)
     bgrect = playerBox(1)
     pygame.draw.rect(screen, (11, 11, 11), bgrect)
 
@@ -54,7 +63,7 @@ while running:
     redbox.rect.y = wc.y - redbox.size/2 + redbox.pos.y - camera.y
 
     redbox.pos += redbox.velocity*dT
-    #print(redbox.pos)
+    #print(redbox.pos) # (debug)
 
     redbox_maxspeed = 150
     movementEasing = 20
@@ -71,15 +80,17 @@ while running:
         if keyboard.is_pressed("d"):
             redbox.velocity.x = lerp(redbox.velocity.x, redbox_maxspeed, movementEasing)
             redbox.velocity.x = min(redbox.velocity.x, redbox_maxspeed)
-        
+
+    #   stupid way of cancelling out velocity since lerping to 0 wasn't working
     else:
-        if redbox.velocity.x > 0.1:
+        if redbox.velocity.x > 0.1: 
             redbox.velocity.x = lerp(redbox.velocity.x, -redbox_maxspeed, movementEasing)
         elif redbox.velocity.x < -0.1:
             redbox.velocity.x = lerp(redbox.velocity.x, redbox_maxspeed, movementEasing)
         else:
             redbox.velocity.x = 0
-            
+
+    #   handles jumping and ground collision, be changed once actual objects are added
     if redbox.pos.y + redbox.size/2 > 0:
         if keyboard.is_pressed("w"):
             redbox.velocity.y = -275
@@ -91,7 +102,7 @@ while running:
     camera.x = lerp(camera.x, -redbox.pos.x*2 - wc.x, cam_easing)
     camera.y = lerp(camera.y, -redbox.pos.y*2 - wc.y + 60, cam_easing)
 
-    #print(camera)
+    #print(camera) # (debug)
     
     pygame.display.flip()
     
