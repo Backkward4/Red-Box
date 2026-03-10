a053873fcb89d2fa6ab38a0d202e26b09b9f9ce0
Backkward <backkward4@gmail.com>
2026-03-09 19:47:56 -0500

    First iteration, I barely know what I'm doing lol

---
+++ Red-Box.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.locals import QUIT
+from pygame.math import Vector2
+
+pygame.init()
+
+window = Vector2(640, 480)
+screen = pygame.display.set_mode((window.x, window.y))
+pygame.display.set_caption("RED BOX")
+center = Vector2(window.x/2, window.y/2)
+
+camera = Vector2(0, 0)
+wc = center + camera
+
+gravity = Vector2(0, -0.91)
+
+class playerBox:
+    def __init__(self, rect):
+        self.size = 40
+        self.rect = pygame.Rect(wc.x - self.size/2, wc.y - self.size/2, self.size, self.size)
+        self.pos = Vector2()
+        self.velocity = Vector2(0, 0)
+
+redbox = playerBox(0)
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT: # Exit when the window is closed
+            running = False
+
+    redbox.velocity -= gravity*0.05
+    
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (251, 53, 38), redbox)
+
+    redbox.rect.x = wc.x - redbox.size/2 + redbox.pos.x
+    redbox.rect.y = wc.y - redbox.size/2 + redbox.pos.y
+
+    redbox.pos += redbox.velocity
+    print(redbox.velocity)
+    #redbox.x += rb_velocity.x
+    #redbox.y += rb_velocity.y
+    
+    pygame.display.flip()
+    
+# Quit PyGame
+pygame.quit()
